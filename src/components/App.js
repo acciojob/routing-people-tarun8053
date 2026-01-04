f4f8200f8de0f4c441aae5629df3f972a6d7347c
Tarun <thakurtarun1012@gmail.com>
2026-01-04 23:19:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,4 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import UserList from "./UserList";
-import UserDetails from "./UserDetails";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<UserList />} />
-      <Route path="/users/:id" element={<UserDetails />} />
-    </Routes>
-  );
-}
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./UserList";
 import UserDetails from "./UserDetails";
@@ -27,5 +14,4 @@ function App() {
   );
 }
 
-export default App;
 export default App;
