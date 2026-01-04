973f08b4731e088cdd81c6f008f233676f2352ea
Tarun <thakurtarun1012@gmail.com>
2026-01-04 23:15:21 +0530

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,24 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import 'regenerator-runtime/runtime'
 
 function UserList() {
-  // 👇 SBI ko turant 1 li chahiye
-  const [users, setUsers] = useState([
-    { id: 1, name: "Leanne Graham" }
-  ]);
+  const [users, setUsers] = useState([]);
+  const [loading,setLoading] = useState(true)
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(data => setUsers(data));
+    async function fetchUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      setUsers(data);
+      setLoading(false)
+    }
+
+    fetchUsers();
   }, []);
+
+  if(loading)return(<h2>Loading...</h2>)
 
   return (
     <div>
       <h1>User List</h1>
       <ul>
-        {users.map(user => (
+        {users.map((user) => (
           <li key={user.id}>
-            <a href={`/users/${user.id}`}>{user.name}</a>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
           </li>
         ))}
       </ul>
