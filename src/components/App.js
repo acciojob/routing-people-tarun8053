88fc135f18c2dccfeecb88f86df847ee10e967b4
Tarun <thakurtarun1012@gmail.com>
2026-01-04 21:39:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,80 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Link, useParams } from "react-router-dom";
 
-const App = () => {
+/* ---------------- App ---------------- */
+
+function App() {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+/* ---------------- User List ---------------- */
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <a href={`/users/${user.id}`}>{user.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+/* ---------------- User Details ---------------- */
+
+function UserDetails() {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+
+      <Link to="/">Back</Link>
+    </div>
+  );
+}
+export default App;
+
