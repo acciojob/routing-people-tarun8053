675c36b170201d62a9e074575bfb75fb14b0dbfc
Tarun <thakurtarun1012@gmail.com>
2026-01-04 23:16:09 +0530

    Update UserDetails.js

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,14 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import 'regenerator-runtime/runtime'
 
 function UserDetails() {
   const { id } = useParams();
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => res.json())
-      .then(data => setUser(data));
+    async function fetchUser() {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+      );
+      const data = await response.json();
+      setUser(data);
+    }
+
+    
+setTimeout(()=>{
+fetchUser();
+},1000)
   }, [id]);
 
   if (!user) {
@@ -17,11 +27,22 @@ function UserDetails() {
 
   return (
     <div>
-      <p>Name: {user.name}</p>
-      <p>Username: {user.username}</p>
-      <p>Email: {user.email}</p>
-      <p>Phone: {user.phone}</p>
-      <p>Website: {user.website}</p>
+      <h1>User Details</h1>
+      <p>
+        <strong>Name:</strong> {user.name}
+      </p>
+      <p>
+        <strong>Username:</strong> {user.username}
+      </p>
+      <p>
+        <strong>Email:</strong> {user.email}
+      </p>
+      <p>
+        <strong>Phone:</strong> {user.phone}
+      </p>
+      <p>
+        <strong>Website:</strong> {user.website}
+      </p>
     </div>
   );
 }
